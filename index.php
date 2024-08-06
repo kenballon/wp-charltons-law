fe4cfb7cf07ed7398060c956a30c44f31fc5a7b2
Ken Ballon <bkenn003@gmail.com>
2024-08-06 15:07:04 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Charltons Law Firm | Hong Kong </title>
+</head>
+
+<body>
+    <header></header>
+    <main>
+        <section class="hero">
+            <h1>Welcome to Charltons Law Firm</h1>
+        </section>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
